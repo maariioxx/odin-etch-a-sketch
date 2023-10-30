1259fee321ee5a22b4b8da76cb3a7477567b3fe7
maariioxx <mariorb2017@gmail.com>
2023-10-30 11:32:04 +0100

    Add change size

---
+++ main.js
@@ -0,0 +1,60 @@
+const grid = document.querySelector(".grid");
+
+const numberButton = document.querySelector(".numberGrid");
+
+let numberSquares;
+
+numberButton.addEventListener('click', () => {
+    
+    changeSize();
+    
+    createGrid(numberSquares);
+    
+    
+
+  
+    
+})
+
+const changeSize = () => {
+    numberSquares = parseInt(prompt("How many squares per side do you want? (Maximum 100)"));
+    while (numberSquares > 100){
+        numberSquares = parseInt(prompt("How many squares per side do you want? (Maximum 100)"));
+    }
+    return numberSquares
+}
+
+
+const createGrid = (numberSquares) => {
+    for (i = 0; i < numberSquares; i++){
+        const row = document.createElement("div");
+        row.classList.add("row");
+        row.style.width = 500 / numberSquares + "px";
+        row.style.height = 500 / numberSquares + "px";
+        grid.appendChild(row);
+        row.addEventListener("mouseover", () => {
+            row.style.backgroundColor = "aquamarine";
+        })
+        for (j = 0; j < numberSquares; j++){
+            const cell = document.createElement("div")
+            cell.classList.add("cell");
+            cell.style.width = 500 / numberSquares + "px";
+            cell.style.height = 500 / numberSquares + "px";
+            grid.appendChild(cell);
+            cell.addEventListener('mouseover', () => {
+                cell.style.backgroundColor = "aquamarine";
+                })
+            }
+    }
+    
+    }
+
+
+    
+createGrid(16);
+
+
+
+
+
+
