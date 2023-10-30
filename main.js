f97b76d254fdf1cf1bf8fb5c400303dfc079d4f2
maariioxx <mariorb2017@gmail.com>
2023-10-30 12:11:29 +0100

    Clean code

--- main.js
+++ main.js
@@ -5,14 +5,7 @@ const numberButton = document.querySelector(".numberGrid");
 let numberSquares;
 
 numberButton.addEventListener('click', () => {
-    
-    clearGrid();
-    
-    
-    
-
-  
-    
+    clearGrid();  
 })
 
 const changeSize = () => {
@@ -20,29 +13,19 @@ const changeSize = () => {
     while (numberSquares > 100){
         numberSquares = parseInt(prompt("How many squares per side do you want? (Maximum 100)"));
     }
-    return numberSquares
+    return numberSquares;
 }
 
 const clearGrid = () => {
     while(grid.firstChild){
-    grid.removeChild(grid.lastChild)
+    grid.removeChild(grid.lastChild);
     }
     changeSize();
     createGrid(numberSquares);
 }
 
-
-
 const createGrid = (numberSquares) => {
     for (i = 0; i < numberSquares; i++){
-        const row = document.createElement("div");
-        row.classList.add("row");
-        row.style.width = 500 / numberSquares + "px";
-        row.style.height = 500 / numberSquares + "px";
-        grid.appendChild(row);
-        row.addEventListener("mouseover", () => {
-            row.style.backgroundColor = "aquamarine";
-        })
         for (j = 0; j < numberSquares; j++){
             const cell = document.createElement("div")
             cell.classList.add("cell");
@@ -50,15 +33,15 @@ const createGrid = (numberSquares) => {
             cell.style.height = 500 / numberSquares + "px";
             grid.appendChild(cell);
             cell.addEventListener('mouseover', () => {
-                cell.style.backgroundColor = "aquamarine";
+                let R = Math.round(Math.random() * 256);
+                let G = Math.round(Math.random() * 256);
+                let B = Math.round(Math.random() * 256);
+                cell.style.backgroundColor = `rgb(${R}, ${G}, ${B})`;
                 })
             }
     }
-    
-    }
+}
 
-
-    
 createGrid(16);
 
 
